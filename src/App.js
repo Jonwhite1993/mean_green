3d796401f83e8a3e77b8c366790f439a8562015a
Jon <jonwhite1993@gmail.com>
2021-06-11 23:20:23 -0600

    Added a main container to house the rest of the ui components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import MainContainer from './components/MainContainer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <MainContainer />
     </div>
   );
 }
---
+++ src/components/MainContainer.js
@@ -0,0 +1,9 @@
+const MainContainer = () => {
+  return(
+    <div className="main-container">
+      Main Container
+    </div>
+  )
+}
+
+export default MainContainer
